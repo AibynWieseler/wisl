2f0869a6cac7c06adb7eee154e5c1eb0c3171fcb
AibynWieseler <wiesler152@gmail.com>
2026-01-25 20:54:25 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,36 @@
+#operators are used to perform operations on variables and values
+
+print(10 + 5)
+
+sum1 = 100 + 50
+sum2 = sum1 + 250
+sum3 = sum2 + sum2
+
+#arithmetic operations
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#comparison operators
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#membership operators are used to test if a sequence is presented in an object
+fruits = ["apple", "banana", "cherry"]
+
+print("banana" in fruits) #to check if banana is in the fruits list
+
