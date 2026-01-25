f876cd46ba8a2b4cee50957d3d67369f7ccae1b1
AibynWieseler <wiesler152@gmail.com>
2026-01-25 20:53:57 +0500

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,19 @@
+#True or False
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+#will return False
+
+#but most values are True
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
